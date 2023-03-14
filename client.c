84217ce9ab42b30b472e163c1aced9aebbc90b47
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-03-14 00:01:19 -0700

    Code refactor

--- client.c
+++ client.c
@@ -21,18 +21,21 @@ struct config{
     int udp_packets;
     int time_to_live;
 };
-struct config* read_config_file(char* path){
+struct config* config;
+struct cJSON *read_config(char* path){
     FILE* fp;
     char buffer[1024];
+
     /* Opening file in reading mode */
     fp = fopen(path, "r");
     fread(buffer,1024,1,fp);
+    
     /* Parse json file */
     cJSON* json = cJSON_Parse(buffer);
-    char* string = cJSON_Print(json);
     fclose(fp);
-    connectToServer(string);
-
+    return json;
+}
+struct config* get_config_struct(cJSON* json){
     const cJSON *server_ip = NULL;
     const cJSON *src_port_udp = NULL;
     const cJSON *dst_port_udp = NULL;
@@ -44,7 +47,7 @@ struct config* read_config_file(char* path){
     const cJSON *udp_packets = NULL;
     const cJSON *time_to_live = NULL;
 
-    /*Parse config fields*/
+    /* Parse config fields */
     server_ip =  cJSON_GetObjectItemCaseSensitive(json, "server_ip");
     src_port_udp =  cJSON_GetObjectItemCaseSensitive(json, "source_port_udp");
     dst_port_udp =  cJSON_GetObjectItemCaseSensitive(json, "destination_port_udp");
@@ -56,7 +59,7 @@ struct config* read_config_file(char* path){
     udp_packets = cJSON_GetObjectItemCaseSensitive(json, "udp_packets");
     time_to_live = cJSON_GetObjectItemCaseSensitive(json, "time_to_live");
 
-    /*Create config struct*/
+    /* Create config struct */
     struct config *config = malloc(sizeof *config);
     strcpy(config->server_ip,server_ip->valuestring);
     config->source_port_udp = src_port_udp->valueint;
@@ -71,16 +74,64 @@ struct config* read_config_file(char* path){
 
     return config;
 }
+void send_config_to_server(char* msg){
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+ 
+    /* socket create */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+ 
+    /* assign IP, PORT */
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr(config->server_ip);
+    servaddr.sin_port = htons(config->tcp_port);
+ 
+    /* connect the client socket to server socket */
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
+        != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+ 
+    char buffer[8000];
+    int n;
+    bzero(buffer,sizeof(buffer));
+    n=0;
+    strcpy(buffer,msg);
+    write(sockfd, buffer, sizeof(buffer));
+    bzero(buffer,sizeof(buffer));
+    close(sockfd);
+}
 void main(int argc, char *args[]){
     if(argc<2){
         printf("Insufficient arguments. Please provide config file.\n");
         return;
     }
-    
-    //char* string = cJSON_Print(json);
+    /*Get config file path from arg*/
     char* config_path = args[1];
-    struct config *config = read_config_file(config_path);
-    /*Parse config file*/
+
+    /*Read config file and get cJSON object*/
+    cJSON* json = read_config(config_path);
+
+    /*Get config values into struct*/
+    config = get_config_struct(json);
+    
+    /*Get config file in a string format*/
+    char * json_str = cJSON_Print(json);
+
+    /*Send config file to server*/
+    send_config_to_server(json_str);
+}
+void print_config(){
     printf("%s\n", config->server_ip);
     printf("%d\n", config->source_port_udp);
     printf("%d\n", config->destination_port_udp);
@@ -91,49 +142,5 @@ void main(int argc, char *args[]){
     printf("%d\n", config->inter_measurement_time);
     printf("%d\n", config->udp_packets);
     printf("%d\n", config->time_to_live);
-    
 }
 
-void connectToServer(char* msg){
-    int sockfd, connfd;
-    struct sockaddr_in servaddr, cli;
- 
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
- 
-    // assign IP, PORT
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("10.0.0.251");
-    servaddr.sin_port = htons(8080);
- 
-    // connect the client socket to server socket
-    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-        != 0) {
-        printf("connection with the server failed...\n");
-        exit(0);
-    }
-    else
-        printf("connected to the server..\n");
- 
-    // function for chat
-    sendMsg(sockfd, msg);
-    close(sockfd);
-}
-
-void sendMsg(int socketfd, char* msg){
-    char buffer[8000];
-    int n;
-    bzero(buffer,sizeof(buffer));
-    printf("Enter string\n");
-    n=0;
-    strcpy(buffer,msg);
-    write(socketfd, buffer, sizeof(buffer));
-    bzero(buffer,sizeof(buffer));
-}
