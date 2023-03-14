f14348159283dc4ce002af88b3d4a752c4cf6b12
gakulkarni <gakulkarni@server.hsd1.ca.comcast.net>
2023-03-13 17:04:06 -0700

    Code clean up

--- server.c
+++ server.c
@@ -6,8 +6,22 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include"cJSON.h"
+#include"cJSON.c"
 #define SA struct sockaddr
 
+struct config{
+    char server_ip[50];
+    int source_port_udp;
+    int destination_port_udp;
+    char destination_port_tcp_head_syn[50];
+    char destination_port_tcp_tail_syn[50];
+    int tcp_port;
+    int udp_payload_size;
+    int inter_measurement_time;
+    int udp_packets;
+    int time_to_live;
+};
 void main(){
     int sockfd, connfd, len;
     struct sockaddr_in servaddr, cli;
@@ -54,36 +68,57 @@ void main(){
         printf("server accept the client...\n");
    
     // Function for chatting between client and server
-    receiveMsg(connfd);
+    struct config *config = receiveMsg(connfd);
    
     // After chatting close the socket
     close(sockfd);
 }
 
-void receiveMsg(int connfd){
-    char buff[80];
+struct config* receiveMsg(int connfd){
+    char buff[8000];
     int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, 80);
-   
-        // read the message from client and copy it in buffer
-        read(connfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, 80);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-   
-        // and send that buffer to client
-        write(connfd, buff, sizeof(buff));
-   
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
+    bzero(buff, 80);
+    // read the message from client and copy it in buffer
+    read(connfd, buff, sizeof(buff));
+    char* output;
+    output = &buff;
+    // print buffer which contains the client contents
+    printf("From client: %s\t", output);
+    //bzero(buff, 80);
+    cJSON* json = cJSON_Parse(output);
+    const cJSON *server_ip = NULL;
+    const cJSON *src_port_udp = NULL;
+    const cJSON *dst_port_udp = NULL;
+    const cJSON *dst_port_tcp_head_syn = NULL;
+    const cJSON *dst_port_tcp_tail_syn = NULL;
+    const cJSON *tcp_port = NULL;
+    const cJSON *udp_payload_size = NULL;
+    const cJSON *inter_measurement_time = NULL;
+    const cJSON *udp_packets = NULL;
+    const cJSON *time_to_live = NULL;
+
+    /*Parse config fields*/
+    server_ip =  cJSON_GetObjectItemCaseSensitive(json, "server_ip");
+    src_port_udp =  cJSON_GetObjectItemCaseSensitive(json, "source_port_udp");
+    dst_port_udp =  cJSON_GetObjectItemCaseSensitive(json, "destination_port_udp");
+    dst_port_tcp_head_syn =  cJSON_GetObjectItemCaseSensitive(json, "destination_port_tcp_head_syn");
+    dst_port_tcp_tail_syn =  cJSON_GetObjectItemCaseSensitive(json, "destination_port_tcp_tail_syn");
+    tcp_port =  cJSON_GetObjectItemCaseSensitive(json, "tcp_port");
+    udp_payload_size =  cJSON_GetObjectItemCaseSensitive(json, "udp_payload_size");
+    inter_measurement_time = cJSON_GetObjectItemCaseSensitive(json,"inter_measurement_time");
+    udp_packets = cJSON_GetObjectItemCaseSensitive(json, "udp_packets");
+    time_to_live = cJSON_GetObjectItemCaseSensitive(json, "time_to_live");
+
+    /*Create config struct*/
+    struct config *config = malloc(sizeof *config);
+    strcpy(config->server_ip,server_ip->valuestring);
+    config->source_port_udp = src_port_udp->valueint;
+    config->destination_port_udp = dst_port_udp->valueint;
+    strcpy(config->destination_port_tcp_head_syn, dst_port_tcp_head_syn->valuestring);
+    strcpy(config->destination_port_tcp_tail_syn, dst_port_tcp_tail_syn->valuestring);
+    config->tcp_port = tcp_port->valueint;
+    config->udp_payload_size = udp_payload_size->valueint;
+    config->inter_measurement_time = inter_measurement_time->valueint;
+    config->udp_packets = udp_packets->valueint;
+    config->time_to_live = time_to_live->valueint;
 }
