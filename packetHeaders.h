27069af0d3878670b4c1a59d6b63f93cfc3bb288
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-03-25 01:04:37 -0700

    Commented code

--- packetHeaders.h
+++ packetHeaders.h
@@ -1,14 +1,14 @@
-struct tcpheader {
- unsigned short int th_sport;
- unsigned short int th_dport;
- unsigned int th_seq;
- unsigned int th_ack;
- unsigned char th_x2:4, th_off:4;
- unsigned char th_flags;
- unsigned short int th_win;
- unsigned short int th_sum;
- unsigned short int th_urp;
-}; /* total tcp header length: 20 bytes (=160 bits) */
+// struct tcpheader {
+//  unsigned short int th_sport;
+//  unsigned short int th_dport;
+//  unsigned int th_seq;
+//  unsigned int th_ack;
+//  unsigned char th_x2:4, th_off:4;
+//  unsigned char th_flags;
+//  unsigned short int th_win;
+//  unsigned short int th_sum;
+//  unsigned short int th_urp;
+// }; /* total tcp header length: 20 bytes (=160 bits) */
 
 // struct ipheader {
 //  unsigned char ip_hl:4, ip_v:4; /* this means that each member is 4 bits */
